55f3bfbf841556d547f5dec0f076fcdadf27745c
jabi2 <boc21211@uvg.edu.gt>
2023-03-13 19:59:29 -0600

    Primera grafica con etiqueta y colores
    Añadi bar chart de pinguinos con las etiquetas de la grafica y colores para tener una idea de las graficas

--- archive/PenguinProyecto.R
+++ archive/PenguinProyecto.R
@@ -7,5 +7,8 @@ library(ggthemes)
 penguins <- read.csv('penguins.csv')
   #Variable de dataset
 
+#Bar Chart Distribucion pinguinos por isla
+theme_set(theme_bw())
 ggplot(penguins, aes(x=factor(Island))) +
-  geom_bar()
+  geom_bar(col='black',fill='lightblue') +
+  labs(title="Distribución de pingüinos por isla", x = 'Isla',y="Pingüinos", caption="Proyecto pingüinos")
