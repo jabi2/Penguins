cc068176f3b1c2dab8463cee7537c0b126f36bff
jabi2 <boc21211@uvg.edu.gt>
2023-03-14 03:38:49 -0600

    Grafico de especies por isla
    Agrege grafico de especies por islaaa

--- archive/PenguinProyecto.R
+++ archive/PenguinProyecto.R
@@ -11,4 +11,14 @@ penguins <- read.csv('penguins.csv')
 theme_set(theme_bw())
 ggplot(penguins, aes(x=factor(Island))) +
   geom_bar(col='black',fill='lightblue') +
-  labs(title="Distribución de pingüinos por isla", x = 'Isla',y="Pingüinos", caption="Proyecto pingüinos")
+  labs(title="Pingüinos por isla", x = 'Isla',y="Pingüinos", caption="Proyecto pingüinos")
+
+# Grafica Especies por isla
+theme_set(theme_bw())
+porEspecie<-penguins %>% group_by(Island,Species) %>% summarise(cuenta=n())
+gg2<- ggplot(porEspecie,aes(y=cuenta, x=factor(Species), fill=Species))
+gg2<- gg2+geom_bar(stat = "identity")
+gg2<- gg2+facet_wrap(facets = ~Island,drop = FALSE, nrow = 1)
+gg2<- gg2+labs(title="Distribución de especies por isla", x = 'Isla',y="Pingüinos", caption="Proyecto pingüinos") +
+  theme(axis.text.x = element_blank())
+plot(gg2)
